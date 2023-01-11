7019c3c39fa68c468cb69c12e8607f4d388a360b
Juan Pablo Torres <Est.JuanPabloTorres@gmail.com>
2023-01-11 16:11:40 -0400

    CodeFormat

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,22 +5,16 @@ import { EditUserComponent } from './edit-user/edit-user.component';
 import { LoginComponent } from './login/login.component';
 import { NewUserComponent } from './new-user/new-user.component';
 import { UserListComponent } from './user-list/user-list.component';
-
 const routes: Routes = [
-
   { path: '', component: AppComponent, pathMatch: "full" },
   { path: 'Login', component: LoginComponent },
   { path: 'newUser', component: NewUserComponent },
   { path: 'userList', component: UserListComponent },
   { path: 'editUser/:id', component: EditUserComponent },
-
-
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-
 export const routingComponents=[NewUserComponent,EditUserComponent,UserListComponent,LoginComponent]
